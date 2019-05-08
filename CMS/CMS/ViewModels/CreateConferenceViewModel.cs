195dce1d97cdc14bd200a553e27df5b3da3bb86e
Andra Cardas <andramalina98@gmail.com>
2019-05-08 22:21:14 +0300

    CreateConference almost done

---
+++ CMS/CMS/ViewModels/CreateConferenceViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CMS.Exceptions;
+using CMS.Models;
+using CMS.Services;
+using CMS.Services.Entities;
+using CMS.Services.Users;
+
+namespace CMS.ViewModels
+{
+    public class CreateConferenceViewModel : ICreateEntityViewModel<Conference>
+    {
+        public Conference conference;
+
+        public string Message { get; }
+        public bool Status { get; }
+        public string Title { get; }
+
+        public CreateConferenceViewModel()
+        {
+            Message = null;
+            Title = "Create";
+        }
+
+        public bool CheckEntity(IEntityService<Conference> service, Conference entity)
+        {
+           
+            try
+            {
+                entity = service.Add(entity);
+            }
+            catch
+            {
+                throw;
+            }
+            return true;
+        }
+
+        
+
+        public CreateConferenceViewModel(bool modelState, Conference conference, ConferenceService service)
+        {
+            if (modelState)
+            {
+               
+                Message = " Registration successful!\n";
+                Status = true;
+            }
+            else
+            {
+                Message = " Invalid request!\n";
+                Status = false;
+            }
+        }
+    }
+}
